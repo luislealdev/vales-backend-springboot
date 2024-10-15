5e9a473bae6b91d232c1452cd05c4e320748e768
Luis Leal <luisrrleal@gmail.com>
2024-10-15 09:35:31 -0500

    First commit

---
+++ src/main/java/com/luisrrleal/vales_backend_springboot/ValesBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.luisrrleal.vales_backend_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValesBackendSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValesBackendSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisrrleal/vales_backend_springboot/ValesBackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisrrleal.vales_backend_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValesBackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
